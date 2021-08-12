a83f1fd2df1dbbd0b2ec18cd169e48f579b7c6ac
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-12 12:08:50 +0530

    added method of win or lose

--- src/com/GamblingSimulation/GamblingSimulation.java
+++ src/com/GamblingSimulation/GamblingSimulation.java
@@ -1,11 +1,22 @@
-package com.GamblingSimulation;
+package com.gamblingsimulation;
 
 public class GamblingSimulation {
 	public static final int stake = 100;
 	public static final int bet = 1;
-
+	
+	public static boolean gamble() {
+		if(Math.random()<0.5) 	
+			return true;
+		else
+			return false;
+	}
+	
 	public static void main(String[] args) {
 		
+		if(gamble())
+			System.out.println("Congrats you've won the bet "+bet +"$");
+		else
+			System.out.println("Sorry, you've lost the bet "+bet +"$");
 	}
 
 }
