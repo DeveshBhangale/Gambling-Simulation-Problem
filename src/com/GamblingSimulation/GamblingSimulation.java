70d21245dd5b8a1c7afeb6ae8b3d00a494ee9fda
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-12 11:45:09 +0530

    added UC1

--- src/com/GamblingSimulation/GamblingSimulation.java
+++ src/com/GamblingSimulation/GamblingSimulation.java
@@ -3,8 +3,8 @@ package com.GamblingSimulation;
 public class GamblingSimulation {
 
 	public static void main(String[] args) {
-		int stake = 100;
-		int bet = 1;
+		final int stake = 100;
+		final int bet = 1;
 
 	}
 
