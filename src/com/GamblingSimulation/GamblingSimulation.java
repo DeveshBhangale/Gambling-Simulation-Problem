9cd2d0990520e12ebe21fd292b86d605b3a7bc5e
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-12 11:55:21 +0530

    initialized two variables stake and bet

--- src/com/GamblingSimulation/GamblingSimulation.java
+++ src/com/GamblingSimulation/GamblingSimulation.java
@@ -1,11 +1,11 @@
 package com.GamblingSimulation;
 
 public class GamblingSimulation {
+	public static final int stake = 100;
+	public static final int bet = 1;
 
 	public static void main(String[] args) {
-		final int stake = 100;
-		final int bet = 1;
-
+		
 	}
 
 }
