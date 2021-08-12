0a4100a8c38da4c5f2d65e6b3ff271c007c4574b
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-08-12 13:04:35 +0530

    [Devesh]: added the gamble logic

--- src/com/GamblingSimulation/GamblingSimulation.java
+++ src/com/GamblingSimulation/GamblingSimulation.java
@@ -1,6 +1,7 @@
 package com.gamblingsimulation;
 
 public class gamblingSimulation {
+	//Use Case - 1
 	public static final int stake = 100;
 	public static final int bet = 1;
 	
@@ -12,11 +13,34 @@ public class gamblingSimulation {
 	}
 	
 	public static void main(String[] args) {
-		
+		double formula = ((double)50/stake) * 100;
+		int win = stake + (int)formula;
+		int lose = -(int)formula;
+		int cnt = 0;
+	
+		//Use Case - 2
 		if(gamble())
 			System.out.println("Congrats you've won the bet "+bet +"$");
 		else
 			System.out.println("Sorry, you've lost the bet "+bet +"$");
+		
+		//Use Case - 3 
+		while(true) {
+			if(gamble())
+				cnt+=1;
+			else
+				cnt-=1;
+				
+			if(cnt == win) {
+				System.out.println("Congrats you've won the and the stake amount is "+win +"$");
+				break;
+			}
+			else if(cnt == lose){
+				System.out.println("Sorry, you've lost the bets and the stake amount is "+ (stake-lose) +"$");
+				break;
+			}
+		}
+		
 	}
 
 }
